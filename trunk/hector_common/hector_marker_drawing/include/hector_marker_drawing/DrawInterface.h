58afb6d7a52ccbe5beefa49d0c7369981a0d41ff
Stefan.Kohlbrecher@gmail.com <Stefan.Kohlbrecher@gmail.com@362d0299-bcd8-93f0-de3f-00335e8b5ee8>
2011-12-12 15:14:09 +0000

    [hector_marker_drawing] -add 3d covariance visualization [axes seem to be wrong atm]

--- trunk/hector_common/hector_marker_drawing/include/hector_marker_drawing/DrawInterface.h
+++ trunk/hector_common/hector_marker_drawing/include/hector_marker_drawing/DrawInterface.h
@@ -39,6 +39,7 @@ public:
   virtual void drawPoint(const Eigen::Vector2f& pointWorldFrame) = 0;
   virtual void drawArrow(const Eigen::Vector3f& poseWorld) = 0;
   virtual void drawCovariance(const Eigen::Vector2f& mean, const Eigen::Matrix2f& cov) = 0;
+  virtual void drawCovariance(const Eigen::Vector3f& mean, const Eigen::Matrix3f& covMatrix) = 0;
 
   virtual void setScale(double scale) = 0;
   virtual void setColor(double r, double g, double b, double a = 1.0) = 0;
