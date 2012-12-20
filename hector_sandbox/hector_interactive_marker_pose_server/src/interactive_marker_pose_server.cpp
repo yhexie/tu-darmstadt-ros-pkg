7a7c109972b3fc843acedb2248ee515321ad80fd
Stefan.Kohlbrecher <Stefan.Kohlbrecher@gmail.com>
2012-12-20 11:39:31 +0000

    [hector_interactive_marker_pose_server] -remove unused code

--- hector_sandbox/hector_interactive_marker_pose_server/src/interactive_marker_pose_server.cpp
+++ hector_sandbox/hector_interactive_marker_pose_server/src/interactive_marker_pose_server.cpp
@@ -84,29 +84,6 @@ ros::Publisher posePublisher_;
 geometry_msgs::PoseStamped out_pose_;
 
 
-// %Tag(frameCallback)%
-void frameCallback(const ros::TimerEvent&)
-{
-  static uint32_t counter = 0;
-
-  static tf::TransformBroadcaster br;
-
-  tf::Transform t;
-
-  ros::Time time = ros::Time::now();
-
-  t.setOrigin(tf::Vector3(0.0, 0.0, sin(float(counter)/140.0) * 2.0));
-  t.setRotation(tf::Quaternion(0.0, 0.0, 0.0, 1.0));
-  br.sendTransform(tf::StampedTransform(t, time, "base_link", "moving_frame"));
-
-  t.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
-  t.setRotation(tf::createQuaternionFromRPY(0.0, float(counter)/140.0, 0.0));
-  br.sendTransform(tf::StampedTransform(t, time, "base_link", "rotating_frame"));
-
-  ++counter;
-}
-// %EndTag(frameCallback)%
-
 // %Tag(processFeedback)%
 void processFeedback( const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
 {
@@ -125,41 +102,11 @@ void processFeedback( const visualization_msgs::InteractiveMarkerFeedbackConstPt
 
   switch ( feedback->event_type )
   {
-    case visualization_msgs::InteractiveMarkerFeedback::BUTTON_CLICK:
-      //ROS_INFO_STREAM( s.str() << ": button click" << mouse_point_ss.str() << "." );
-      break;
-
-    case visualization_msgs::InteractiveMarkerFeedback::MENU_SELECT:
-      //ROS_INFO_STREAM( s.str() << ": menu item " << feedback->menu_entry_id << " clicked" << mouse_point_ss.str() << "." );
-      break;
-
     case visualization_msgs::InteractiveMarkerFeedback::POSE_UPDATE:
-      /*
-      ROS_INFO_STREAM( s.str() << ": pose changed"
-          << "\nposition = "
-          << feedback->pose.position.x
-          << ", " << feedback->pose.position.y
-          << ", " << feedback->pose.position.z
-          << "\norientation = "
-          << feedback->pose.orientation.w
-          << ", " << feedback->pose.orientation.x
-          << ", " << feedback->pose.orientation.y
-          << ", " << feedback->pose.orientation.z
-          << "\nframe: " << feedback->header.frame_id
-          << " time: " << feedback->header.stamp.sec << "sec, "
-          << feedback->header.stamp.nsec << " nsec" );
-          */
+
         out_pose_.pose = feedback->pose;
         out_pose_.header = feedback->header;
         posePublisher_.publish(out_pose_);
-      break;
-
-    case visualization_msgs::InteractiveMarkerFeedback::MOUSE_DOWN:
-      //ROS_INFO_STREAM( s.str() << ": mouse down" << mouse_point_ss.str() << "." );
-      break;
-
-    case visualization_msgs::InteractiveMarkerFeedback::MOUSE_UP:
-      //ROS_INFO_STREAM( s.str() << ": mouse up" << mouse_point_ss.str() << "." );
       break;
   }
 
@@ -189,12 +136,6 @@ void alignMarker( const visualization_msgs::InteractiveMarkerFeedbackConstPtr &f
   server->applyChanges();
 }
 // %EndTag(alignMarker)%
-
-double rand( double min, double max )
-{
-  double t = (double)rand() / (double)RAND_MAX;
-  return min + t*(max-min);
-}
 
 // %Tag(Box)%
 Marker makeBox( InteractiveMarker &msg )
@@ -293,239 +234,6 @@ void make6DofMarker( bool fixed )
 }
 // %EndTag(6DOF)%
 
-/*
-// %Tag(RandomDof)%
-void makeRandomDofMarker( )
-{
-  InteractiveMarker int_marker;
-  int_marker.header.frame_id = "/base_link";
-  int_marker.pose.position.y = -3.0 * marker_pos++;;
-  int_marker.scale = 1;
-
-  int_marker.name = "6dof_random_axes";
-  int_marker.description = "6-DOF\n(Arbitrary Axes)";
-
-  makeBoxControl(int_marker);
-
-  InteractiveMarkerControl control;
-
-  for ( int i=0; i<3; i++ )
-  {
-    control.orientation.w = rand(-1,1);
-    control.orientation.x = rand(-1,1);
-    control.orientation.y = rand(-1,1);
-    control.orientation.z = rand(-1,1);
-    control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-    int_marker.controls.push_back(control);
-    control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-    int_marker.controls.push_back(control);
-  }
-
-  server->insert(int_marker);
-  server->setCallback(int_marker.name, &processFeedback);
-}
-// %EndTag(RandomDof)%
-
-
-// %Tag(ViewFacing)%
-void makeViewFacingMarker( )
-{
-  InteractiveMarker int_marker;
-  int_marker.header.frame_id = "/base_link";
-  int_marker.pose.position.y = -3.0 * marker_pos++;;
-  int_marker.scale = 1;
-
-  int_marker.name = "view_facing";
-  int_marker.description = "View Facing 6-DOF";
-
-  InteractiveMarkerControl control;
-
-  // make a control that rotates around the view axis
-  control.orientation_mode = InteractiveMarkerControl::VIEW_FACING;
-  control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-  control.orientation.w = 1;
-  control.name = "rotate";
-
-  int_marker.controls.push_back(control);
-
-  // create a box in the center which should not be view facing,
-  // but move in the camera plane.
-  control.orientation_mode = InteractiveMarkerControl::VIEW_FACING;
-  control.interaction_mode = InteractiveMarkerControl::MOVE_PLANE;
-  control.independent_marker_orientation = true;
-  control.name = "move";
-
-  control.markers.push_back( makeBox(int_marker) );
-  control.always_visible = true;
-
-  int_marker.controls.push_back(control);
-
-  server->insert(int_marker);
-  server->setCallback(int_marker.name, &processFeedback);
-}
-// %EndTag(ViewFacing)%
-
-
-// %Tag(Quadrocopter)%
-void makeQuadrocopterMarker( )
-{
-  InteractiveMarker int_marker;
-  int_marker.header.frame_id = "/base_link";
-  int_marker.pose.position.y = -3.0 * marker_pos++;;
-  int_marker.scale = 1;
-
-  int_marker.name = "quadrocopter";
-  int_marker.description = "Quadrocopter";
-
-  makeBoxControl(int_marker);
-
-  InteractiveMarkerControl control;
-
-  control.orientation.w = 1;
-  control.orientation.x = 0;
-  control.orientation.y = 1;
-  control.orientation.z = 0;
-  control.interaction_mode = InteractiveMarkerControl::MOVE_ROTATE;
-  int_marker.controls.push_back(control);
-  control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-  int_marker.controls.push_back(control);
-
-  server->insert(int_marker);
-  server->setCallback(int_marker.name, &processFeedback);
-}
-// %EndTag(Quadrocopter)%
-
-// %Tag(ChessPiece)%
-void makeChessPieceMarker( )
-{
-  InteractiveMarker int_marker;
-  int_marker.header.frame_id = "/base_link";
-  int_marker.pose.position.y = -3.0 * marker_pos++;;
-  int_marker.scale = 1;
-
-  int_marker.name = "chess_piece";
-  int_marker.description = "Chess Piece\n(2D Move + Alignment)";
-
-  InteractiveMarkerControl control;
-
-  control.orientation.w = 1;
-  control.orientation.x = 0;
-  control.orientation.y = 1;
-  control.orientation.z = 0;
-  control.interaction_mode = InteractiveMarkerControl::MOVE_PLANE;
-  int_marker.controls.push_back(control);
-
-  // make a box which also moves in the plane
-  control.markers.push_back( makeBox(int_marker) );
-  control.always_visible = true;
-  int_marker.controls.push_back(control);
-
-  // we want to use our special callback function
-  server->insert(int_marker);
-  server->setCallback(int_marker.name, &processFeedback);
-
-  // set different callback for POSE_UPDATE feedback
-  server->setCallback(int_marker.name, &alignMarker, visualization_msgs::InteractiveMarkerFeedback::POSE_UPDATE );
-}
-// %EndTag(ChessPiece)%
-
-// %Tag(PanTilt)%
-void makePanTiltMarker( )
-{
-  InteractiveMarker int_marker;
-  int_marker.header.frame_id = "/base_link";
-  int_marker.pose.position.y = -3.0 * marker_pos++;;
-  int_marker.scale = 1;
-
-  int_marker.name = "pan_tilt";
-  int_marker.description = "Pan / Tilt";
-
-  makeBoxControl(int_marker);
-
-  InteractiveMarkerControl control;
-
-  control.orientation.w = 1;
-  control.orientation.x = 0;
-  control.orientation.y = 1;
-  control.orientation.z = 0;
-  control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-  control.orientation_mode = InteractiveMarkerControl::FIXED;
-  int_marker.controls.push_back(control);
-
-  control.orientation.w = 1;
-  control.orientation.x = 0;
-  control.orientation.y = 0;
-  control.orientation.z = 1;
-  control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-  control.orientation_mode = InteractiveMarkerControl::INHERIT;
-  int_marker.controls.push_back(control);
-
-  server->insert(int_marker);
-  server->setCallback(int_marker.name, &processFeedback);
-}
-// %EndTag(PanTilt)%
-
-// %Tag(Menu)%
-void makeMenuMarker()
-{
-  InteractiveMarker int_marker;
-  int_marker.header.frame_id = "/base_link";
-  int_marker.pose.position.y = -3.0 * marker_pos++;;
-  int_marker.scale = 1;
-
-  int_marker.name = "context_menu";
-  int_marker.description = "Context Menu\n(Right Click)";
-
-  InteractiveMarkerControl control;
-
-  // make one control using default visuals
-  control.interaction_mode = InteractiveMarkerControl::MENU;
-  control.description="Options";
-  control.name = "menu_only_control";
-  int_marker.controls.push_back(control);
-
-  // make one control showing a box
-  Marker marker = makeBox( int_marker );
-  control.markers.push_back( marker );
-  control.always_visible = true;
-  int_marker.controls.push_back(control);
-
-  server->insert(int_marker);
-  server->setCallback(int_marker.name, &processFeedback);
-  menu_handler.apply( *server, int_marker.name );
-}
-// %EndTag(Menu)%
-
-// %Tag(Moving)%
-void makeMovingMarker()
-{
-  InteractiveMarker int_marker;
-  int_marker.header.frame_id = "/moving_frame";
-  int_marker.pose.position.y = -3.0 * marker_pos++;;
-  int_marker.scale = 1;
-
-  int_marker.name = "moving";
-  int_marker.description = "Marker Attached to a\nMoving Frame";
-
-  InteractiveMarkerControl control;
-
-  control.orientation.w = 1;
-  control.orientation.x = 1;
-  control.orientation.y = 0;
-  control.orientation.z = 0;
-  control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-  int_marker.controls.push_back(control);
-
-  control.interaction_mode = InteractiveMarkerControl::MOVE_PLANE;
-  control.always_visible = true;
-  control.markers.push_back( makeBox(int_marker) );
-  int_marker.controls.push_back(control);
-
-  server->insert(int_marker);
-  server->setCallback(int_marker.name, &processFeedback);
-}
-// %EndTag(Moving)%
-*/
 
 // %Tag(main)%
 int main(int argc, char** argv)
@@ -542,24 +250,7 @@ int main(int argc, char** argv)
 
   ros::Duration(0.1).sleep();
 
-  /*
-  menu_handler.insert( "First Entry", &processFeedback );
-  menu_handler.insert( "Second Entry", &processFeedback );
-  interactive_markers::MenuHandler::EntryHandle sub_menu_handle = menu_handler.insert( "Submenu" );
-  menu_handler.insert( sub_menu_handle, "First Entry", &processFeedback );
-  menu_handler.insert( sub_menu_handle, "Second Entry", &processFeedback );
-  */
   make6DofMarker( false );
-  /*
-  make6DofMarker( true );
-  makeRandomDofMarker( );
-  makeViewFacingMarker( );
-  makeQuadrocopterMarker( );
-  makeChessPieceMarker( );
-  makePanTiltMarker( );
-  makeMenuMarker( );
-  makeMovingMarker( );
-  */
 
   server->applyChanges();
 
